6ef36a621a7cbf1248e99f2ac33382489d07fa7e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-03-10 14:21:01 +0100

    test: update tests

--- src/lib/check.test.js
+++ src/lib/check.test.js
@@ -17,44 +17,76 @@ describe("Info checker", async () => {
 	});
 
 	it("finds company data", () => {
-		// console.log(foundCompanyData)
 		expect(foundCompanyData).toBeDefined();
 	});
 
 	test("companies in medverkande length is equal to companies in classroom length", () => {
-		expect(foundMedverkande.split(",").length).toEqual(foundCompaniesInClassrooms.split(",").length);
-  });
-  
-  test("companies in main info length is equal to companies in classroom length", () => {
+		expect(foundMedverkande.split(",").length).toEqual(
+			foundCompaniesInClassrooms.split(",").length
+		);
+	});
+
+	test("companies in main info length is equal to companies in classroom length", () => {
 		expect(foundCompanyData.length).toEqual(foundCompaniesInClassrooms.split(",").length);
 	});
-  
-  describe("Test if company inside classroom is found in medverkande", async () => {
-		test.each(foundCompaniesInClassrooms.split(",").sort())("%s is found in medverkande", (company) => {
-			expect(foundMedverkande.includes(company)).toBeTruthy();
-		});
-	});
 
-	describe("Test if company inside medverkande is found in a classroom", async () => {
-		test.each(foundMedverkande.split(","))("%s is found in a classroom", (company) => {
-			expect(foundCompaniesInClassrooms.includes(company)).toBeTruthy();
-		});
-	});
-
-	describe("Test if company inside main info is found in a classroom", async () => {
-		test.each(foundCompanyData)(
-			"name in info for $companyName is found in classroom",
-			(company) => {
-				expect(foundCompaniesInClassrooms.includes(company["companyName"].toString())).toBeTruthy();
+	describe("Test if classroom name equals name in medverkande", async () => {
+		test.each(foundCompaniesInClassrooms.split(",").sort())(
+			"%s (classroom) name is equal to name in medverkande",
+			(classroomCompany) => {
+				let found = false;
+				foundMedverkande.split(",").forEach((medverkandeCompany) => {
+					if (medverkandeCompany === classroomCompany) {
+						found = true;
+					}
+				});
+				expect(found).toBeTruthy();
 			}
 		);
 	});
 
-	describe("Test if company inside main info is found in a medverkande", async () => {
+	describe("Test if medverkande name equals name in classroom", async () => {
+		test.each(foundMedverkande.split(","))(
+			"%s (medverkande) name is equal to name in medverkande",
+			(medverkandeCompany) => {
+				let found = false;
+				foundCompaniesInClassrooms.split(",").forEach((classroomCompany) => {
+					if (medverkandeCompany === classroomCompany) {
+						found = true;
+					}
+				});
+				expect(found).toBeTruthy();
+			}
+		);
+	});
+
+	describe("Test if company info name equals name in classroom", async () => {
+		test.each(foundCompanyData)(
+			"name in info for $companyName is found in classroom",
+			(companyInfo) => {
+				let found = false;
+				let currentCompany = companyInfo["companyName"];
+				foundCompaniesInClassrooms.split(",").forEach((classroomCompany) => {
+					if (companyInfo["companyName"] === classroomCompany) {
+						found = true;
+					}
+				});
+				expect(found).toBeTruthy();
+			}
+		);
+	});
+
+	describe("Test if company info name equals name in medverkande", async () => {
 		test.each(foundCompanyData)(
 			"name in info for $companyName is found in medverkande",
-			(company) => {
-				expect(foundMedverkande.includes(company["companyName"].toString())).toBeTruthy();
+			(companyInfo) => {
+				let found = false;
+				foundMedverkande.split(",").forEach((medverkandeCompany) => {
+					if (companyInfo["companyName"] === medverkandeCompany) {
+						found = true;
+					}
+				});
+				expect(found).toBeTruthy();
 			}
 		);
 	});
