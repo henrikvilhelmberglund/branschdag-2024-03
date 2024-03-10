20dfebd5f3a84c4c82e219a0d9a88da6f8de1cff
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-03-10 16:14:45 +0100

    Updates

---
+++ _app/immutable/chunks/2-bf042452.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-2244130e.js";export{t as component};
--- _app/immutable/chunks/2-dace1c36.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-1eb1ca62.js";export{t as component};
